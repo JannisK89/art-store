219e58d75199d81ebf9a43b2aa1ac6f8c67d5ccf
JannisK89 <jannis.karanikis@gmail.com>
2024-05-28 19:49:12 +0200

    playwright setup

---
+++ playwright/main-flow.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test'
+
+test('should open the home page', async ({ page }) => {
+  await page.goto('/')
+
+  await expect(page.getByText('Art Store')).toBeVisible()
+  await expect(page.getByText('Easy')).toBeVisible()
+})
