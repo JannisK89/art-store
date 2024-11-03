1ac5f5e8fcbc90ad055cff16c9726c6da913d735
JannisK89 <jannis.karanikis@gmail.com>
2024-11-03 12:02:07 +0100

    removed slowloading gif

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image'
 import { navigate } from '../actions/actions'
 import { useEffect } from 'react'
 
@@ -11,24 +10,21 @@ export default function Confirmation() {
     }, 7000)
   }, [])
   return (
-    <main className="flex min-h-screen flex-col items-center p-12">
-      <h1 className=" mt-2 text-4xl">Thank you for using Art Store!</h1>
-      <p className="md:w-1/2 text-lg mt-2">
-        Your order has been confirmed and will be delivered once our AI achieves
-        sentience.
-      </p>
-      <p className="text-lg md:w-1/2 mt-2">
-        Please be prepared for an array of futuristic payment methods that may
-        include, but are not limited to, mind-melding transactions,
-        interdimensional banking, or telepathic transfers.
-      </p>
-      <Image
-        src="https://c.tenor.com/TMrEyOZ2PwIAAAAC/tenor.gif"
-        alt="Bender from Futurama dancing"
-        width={300}
-        height={300}
-        className="rounded-xl shadow mt-4"
-      />
+    <main className="flex min-h-screen w-full justify-center p-12">
+      <div className="flex flex-col items-center">
+        <h1 className=" mt-2 text-4xl items-start">
+          Thank you for using Art Store!
+        </h1>
+        <p className="md:w-1/2 text-lg mt-8">
+          Your order has been confirmed and will be delivered once our AI
+          achieves sentience.
+        </p>
+        <p className="text-lg md:w-1/2 mt-2">
+          Please be prepared for an array of futuristic payment methods that may
+          include, but are not limited to, mind-melding transactions,
+          interdimensional banking, or telepathic transfers.
+        </p>
+      </div>
     </main>
   )
 }
